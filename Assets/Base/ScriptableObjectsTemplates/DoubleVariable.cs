aa4f0597d2b46229937b9b961fc47ae3c34795e6
Thomas Abel <thomabel@pdx.edu>
2022-04-04 16:20:58 -0700

    readd

--- Assets/Base/ScriptableObjectsTemplates/DoubleVariable.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu]
-public class DoubleVariable : ScriptableObject
-{
-    public double Value;
-}
--- Assets/Base/ScriptableObjectsTemplates/IntVariable.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu]
-public class IntVariable : ScriptableObject
-{
-    public int Value;
-}
