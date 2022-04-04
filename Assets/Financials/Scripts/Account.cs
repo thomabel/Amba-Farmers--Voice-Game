65c103fc33780c31f75da9374ebd42c0535a06b3
Thomas Abel <thomabel@pdx.edu>
2022-04-04 14:32:46 -0700

    Account SO

--- Assets/Financials/Scripts/Account.cs
+++ Assets/Financials/Scripts/Account.cs
@@ -1,6 +1,36 @@
 using UnityEngine;
+using Currency = System.Int32;
 
-public class Account : MonoBehaviour
+[CreateAssetMenu]
+public class Account : ScriptableObject
 {
-    
+    [SerializeField] private Currency balance;
+    [SerializeField] private Currency debt_floor;
+    [SerializeField] private float annual_rate;
+    [SerializeField] private int annual_periods;
+
+    public Currency Balance()
+    {
+        return balance;
+    }
+    public Currency Credit(Currency amount)
+    {
+        if (balance + amount <= int.MaxValue)
+        {
+            balance += amount;
+        }
+        return balance;
+    }
+    public int Debit(Currency amount)
+    {
+        if (balance - amount >= debt_floor)
+        {
+            balance -= amount;
+        }
+        return balance;
+    }
+    public Currency Interest()
+    {
+        return Credit((int)(annual_rate / annual_periods * balance));
+    }
 }
