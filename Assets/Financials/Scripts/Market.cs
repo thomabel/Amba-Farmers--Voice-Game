db35174a13264ecb8f9effe22c7e2769048c83fa
Thomas Abel <thomabel@pdx.edu>
2022-04-09 19:14:08 -0700

    market basics

--- Assets/Financials/Scripts/Market.cs
+++ Assets/Financials/Scripts/Market.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Market : MonoBehaviour
 {
-    
+    [SerializeField] List<MarketWrapper> inventory;
+
+    public List<MarketWrapper>.Enumerator GetEnumerator()
+    {
+        return inventory.GetEnumerator();
+    }
+    public MarketWrapper GetItem(int index)
+    {
+        return inventory[index];
+    }
 }
