24e6ee8682ec3d0346b91f6b5e275ae3ed9fe17f
Thomas Abel <thomabel@pdx.edu>
2022-05-03 22:19:35 -0700

    fix hoe

--- Assets/Base/Scripts/Hoe.cs
+++ Assets/Base/Scripts/Hoe.cs
@@ -4,9 +4,18 @@ public class Hoe : MonoBehaviour, IEquippable, IInteractable
 {
     void IEquippable.Use(GameObject with)
     {
+        if (with == null)
+        {
+            return;
+        }
         var land = with.GetComponent<TerrainData>();
         if (land != null)
         {
+            land.SetTilled(transform.position, true);
+        }
+        else
+        {
+            land = Terrain.activeTerrain.GetComponent<TerrainData>();
             land.SetTilled(transform.position, true);
         }
     }
