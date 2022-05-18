8e7d87bc60eaf79a617120584c0f85dfa5b56c12
Sandy Wood <sandyaspenwood@gmail.com>
2022-05-18 10:42:19 -0700

    Revert "Merge remote-tracking branch 'origin/main' into plants"
    This reverts commit e2409a85242d1a9a8d8776de527f5c492ee8db0e, reversing
    changes made to 8c714a8dc1bfe8b468e99d24c3dac20ee6820ab5.

--- Assets/Base/Scripts/Hoe.cs
+++ Assets/Base/Scripts/Hoe.cs
@@ -11,9 +11,7 @@ public class Hoe : MonoBehaviour, IEquippable, IInteractable
         }
         else
         {
-            //var land = with.GetComponent<TerrainData>();
-            //land.SetTilled(transform.position, true);
-            var land = Terrain.activeTerrain.GetComponent<TerrainData>();
+            var land = with.GetComponent<TerrainData>();
             land.SetTilled(transform.position, true);
         }
     }
